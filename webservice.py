f87a02331a7ebc0fa495e924f98a99fb92996bc6
mehDktb <mehdikatibeh81@gmail.com>
2025-12-02 18:35:55 +0330

    FIX[app]: the webservice calls the correct function of Qwen now!

--- webservice.py
+++ webservice.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
-from models.Qwen3_VL_8B_Instruct import qwen_chat
+from models.Qwen3_VL_8B_Instruct import qwen_chat, qwen_sql_from_nl
 from models.Llama_31_8B_Instruct import llama_chat
 from models.gpt_oss_20B import gpt_oss_chat
 
 app = Flask(__name__)
 
 MODEL_ROUTER = {
-    "qwen": qwen_chat,
+    "qwen": qwen_sql_from_nl,
     "llama_3.1_8B" : llama_chat,
     "gpt_oss": gpt_oss_chat,
     # later: "llama": llama_chat, etc.
