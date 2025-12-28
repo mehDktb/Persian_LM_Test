7373045a2ed4f2299817f4582116c977f78d4f6b
mehDktb <mehdikatibeh81@gmail.com>
2025-12-28 12:51:20 +0330

    UPDATE[app]: now we can set the system content for gpt.

--- fine_tuning/gpt_dataset_answer_generator.py
+++ fine_tuning/gpt_dataset_answer_generator.py
@@ -1,6 +1,6 @@
 import os
 from openai import OpenAI
-from system_contents.Qwen_sql_system_content import SQL_SYSTEM_PROMPT
+from system_contents.Qwen_sql_system_content import SQL_SYSTEM_PROMPT, GPT_DATASET_GENERATOR
 
 api_key = os.environ.get("OPENAI_API_KEY_ECC")
 if api_key is None:
@@ -10,19 +10,31 @@ if api_key is None:
 
 client = OpenAI(api_key=api_key)
 
-def ask_openai(prompt: str, model: str = "gpt-5.2") -> str:
-    """
-    Sends a system prompt and a user question to an OpenAI model
-    and returns the model's response as a string.
-    """
+def ask_openai(prompt: str, model: str = "gpt-5.2", system_content: str = SQL_SYSTEM_PROMPT) -> str:
 
     response = client.chat.completions.create(
         model=model,
         messages=[
-            {"role": "system", "content": SQL_SYSTEM_PROMPT},
+            {"role": "system", "content": system_content},
             {"role": "user", "content": prompt}
         ],
         temperature=0.0
     )
 
     return response.choices[0].message.content.strip()
+
+
+def main():
+    user_action = input("which system content do want to use?\n1)sql_generation\t2)dataset_generation")
+    if user_action not in [1,2]:
+        raise ValueError("You have to only enter 1 or 2")
+    elif user_action == 1:
+        system_content = SQL_SYSTEM_PROMPT
+    else :
+        system_content = GPT_DATASET_GENERATOR
+    prompt = input("what do you want to ask from GPT?\n->")
+    response = ask_openai(prompt=prompt, system_content=system_content)
+
+    print(response)
+if __name__ == "__main__":
+    main()
